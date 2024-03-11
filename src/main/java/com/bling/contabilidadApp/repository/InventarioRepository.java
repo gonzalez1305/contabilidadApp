1473ef037976e7b18ecdc99910eb87e9c335e82c
HoruKujikawa <wislado151@gmail.com>
2024-03-10 22:38:28 -0500

    eliminacion de archivos

--- src/main/java/com/bling/contabilidadApp/repository/InventarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bling.contabilidadApp.repository;
-
-import com.bling.contabilidadApp.Entity.Inventario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InventarioRepository extends JpaRepository<Inventario, Long> {
-}
